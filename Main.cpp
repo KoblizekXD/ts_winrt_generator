03f1bc6cdcf12dfe756289f57c61c3075b034561
KoblizekXD <janprokupek@centrum.cz>
2024-10-19 09:37:51 +0200

    Make output pumpable into file & update gitignore for temp files

--- Main.cpp
+++ Main.cpp
@@ -4,25 +4,29 @@
 int main(int argc, char** argv)
 {
 	if (argc < 2) {
-		std::cerr << "Usage: " << argv[0] << " <winmd file>" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " <winmd file> [output]" << std::endl;
 		return EXIT_FAILURE;
 	}
 
 	try
 	{
 		winmd::reader::database db(argv[1]);
+		FILE* stream = (argc == 3 ? fopen(argv[2], "w+") : stdout);
 
-		printf("\n=================================================\n");
-		printf("\nWindows Metadata Typescript definition generator.\n");
+		printf("Output will be pumped into %s\n\n", (argc == 3 ? argv[2] : "stdout"));
+
+		printf("=================================================\n");
+		printf("Windows Metadata Typescript definition generator.\n");
 		printf("Revision 0v1.0. Licensed under MIT.\n");
-		printf("\n=================================================\n");
-
+		printf("=================================================\n\n");
+		
 		for (size_t i = 0; i < db.TypeDef.size(); i++)
 		{
 			auto row = db.TypeDef[i];
-			std::cout << row.TypeName() << std::endl;
+			fprintf(stream, "%s\n", row.TypeName().data());
 		}
 
+		fclose(stream);
 	}
 	catch (const std::invalid_argument& e)
 	{
