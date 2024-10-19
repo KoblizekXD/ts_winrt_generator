b48dde7a9a2500368d5a25566aaf49775e5443f2
KoblizekXD <janprokupek@centrum.cz>
2024-10-19 09:25:27 +0200

    Use CLI params to obtain file(instead of hardcoded)

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,33 @@
 #include "pch.h"
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
-	winmd::reader::database db("C:\\Users\\Koblizkac\\Documents\\Dev\\CXX\\EmptyWinUI3\\x64\\Debug\\Microsoft.UI.Xaml.winmd");
-	for (size_t i = 0; i < db.TypeDef.size(); i++)
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " <winmd file>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	try
 	{
-		auto row = db.TypeDef[i];
-		std::cout << row.TypeName() << std::endl;
+		winmd::reader::database db(argv[1]);
+
+		printf("\n=================================================\n");
+		printf("\nWindows Metadata Typescript definition generator.\n");
+		printf("Revision 0v1.0. Licensed under MIT.\n");
+		printf("\n=================================================\n");
+
+		for (size_t i = 0; i < db.TypeDef.size(); i++)
+		{
+			auto row = db.TypeDef[i];
+			std::cout << row.TypeName() << std::endl;
+		}
+
+	}
+	catch (const std::invalid_argument& e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 }
